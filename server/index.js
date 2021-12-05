4280ed004eaf47d50ec78112999f5f2395eb96a5
Alex Ni <cl.alexni@gmail.com>
2021-12-04 23:03:00 -0600

    setup file structures and server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname  + '/../dist'));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Server running at http://localhost:${PORT}`);
+  }
+});
